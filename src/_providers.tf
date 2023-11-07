ec8999fa1498ab4a44114951704291b60103ca32
NonpareilNic <nickbenton07@gmail.com>
2023-11-07 15:26:33 -0500

    Final sweep for first PR of public bundle and terraform formatting.

--- src/_providers.tf
+++ src/_providers.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "aws" {
-  region     = var.region
+  region = var.region
   assume_role {
     role_arn    = var.aws_authentication.data.arn
     external_id = var.aws_authentication.data.external_id
@@ -18,36 +18,3 @@ provider "aws" {
     tags = var.md_metadata.default_tags
   }
 }
-
-# provider "azurerm" {
-#   features {}
-
-#   client_id       = var.azure_service_principal.data.client_id
-#   tenant_id       = var.azure_service_principal.data.tenant_id
-#   client_secret   = var.azure_service_principal.data.client_secret
-#   subscription_id = var.azure_service_principal.data.subscription_id
-# }
-
-# provider "google" {
-#   project     = var.gcp_authentication.data.project_id
-#   credentials = jsonencode(var.gcp_authentication.data)
-#   region      = var.mrc.specs.gcp.region
-# }
-
-# provider "google-beta" {
-#   project     = var.gcp_authentication.data.project_id
-#   credentials = jsonencode(var.gcp_authentication.data)
-#   region      = var.mrc.specs.gcp.region
-# }
-
-# provider "helm" {
-#   kubernetes {
-#     config_context = "default"
-#     config_path = "kube.yaml"
-#   }
-# }
-
-# provider "kubernetes" {
-#   config_context = "default"
-#   config_path = "kube.yaml"
-# }
--- src/main.tf
+++ src/main.tf
@@ -2,13 +2,13 @@ resource "aws_sagemaker_endpoint_configuration" "main" {
   name = "${var.md_metadata.name_prefix}-endpoint-config"
 
   production_variants {
-    model_name = var.endpoint_config.model_name
-    instance_type = var.endpoint_config.instance_type
+    model_name             = var.endpoint_config.model_name
+    instance_type          = var.endpoint_config.instance_type
     initial_instance_count = var.endpoint_config.instance_count
   }
 }
 
 resource "aws_sagemaker_endpoint" "main" {
-  name = "${var.md_metadata.name_prefix}-endpoint"
+  name                 = "${var.md_metadata.name_prefix}-endpoint"
   endpoint_config_name = aws_sagemaker_endpoint_configuration.main.name
 } 
